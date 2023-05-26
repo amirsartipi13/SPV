d4271dcf7440adb0a90ef86d8e4ce522bb725f96
Amir <40461594+amirsartipi13@users.noreply.github.com>
2023-05-26 18:55:41 +0330

    add pprint

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
+from pprint import pprint
 
 
 class SolarModel(pl.LightningModule):
